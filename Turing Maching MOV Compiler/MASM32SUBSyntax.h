582eb4489685dfb381040b42a40b4ae941827933
idoheinemann <idohaineman@gmail.com>
2020-05-24 22:30:28 +0300

    fixed bugs in xor and sub

--- Turing Maching MOV Compiler/MASM32SUBSyntax.h
+++ Turing Maching MOV Compiler/MASM32SUBSyntax.h
@@ -6,18 +6,15 @@ protected:
 	void sub(string dst, string src, string& code) {
 		code += "SUB " + dst + ", " + src + "\n";
 	}
-	void zero(string& code) {
-		sub("esp", "esp", code); // esp = 0
-		sub("ebp", "ebp", code); // ebp = 0
-	}
 public:
 	void mov(string dst, string src, string& code) {
-		zero(code); // esp = ebp = 0
-		sub("esp", dst, code); // esp = -dst
-		sub("ebp", "esp", code); // ebp = 0 - (-dst) = dst
-		sub(dst, "ebp", code); // dst = 0
-		zero(code); // esp = ebp = 0
+		sub("ebp", "ebp", code); // ebp = 0
+		sub("esp", "esp", code); // esp = 0
+		sub("ebp", dst, code); // ebp = -dst
+		sub("esp", "ebp", code); // esp = dst
+		sub("ebp", "ebp", code); // ebp = 0
 		sub("ebp", src, code); // ebp = -src
-		sub(dst, "ebp", code); // dst = 0 - (-src) = src
+		sub(dst, "esp", code); // dst = 0
+		sub(dst, "ebp", code); // dst = src
 	}
 };
--- Turing Maching MOV Compiler/MASM32XORSyntax.h
+++ Turing Maching MOV Compiler/MASM32XORSyntax.h
@@ -10,11 +10,11 @@ protected:
 	}
 public:
 	void mov(string dst, string src, string& code) {
-		XOR("ebp", "ebp", code);
+		XOR("ebp", "ebp", code); // ebp = 0
+		XOR("esp", "esp", code); // esp = 0
+		XOR("esp", src, code); // esp = src
 		XOR("ebp", dst, code); // ebp = dst
 		XOR(dst, "ebp", code); // dst = 0
-		XOR("ebp", "ebp", code);
-		XOR("ebp", src, code); // ebp = src
-		XOR(dst, "ebp", code);
+		XOR(dst, "esp", code); // dst = src
 	}
 };
